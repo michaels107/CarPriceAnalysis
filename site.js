f471dbd04afb79a93af80c30eebe1b88f0f75076
nguyen.1691 <nguyen.1691@osu.edu>
2020-11-13 21:01:52 -0500

    additional text box information

--- site.js
+++ site.js
@@ -1,3 +1,16 @@
-/*Print points onto page*/
-document.getElementById("Enter").innerHtml = "Point Total: " + points;
+let make = document.getElementById("make").value;
+console.log(make);
+let model = document.getElementById("model").value;
+let currentprice = document.getElementById("currentprice").value;
+let tax = document.getElementById("tax").value;
+let titles = document.getElementById("titles").value;
+let fees = document.getElementById("fees").value;
+let miscellaneoustaxes = document.getElementById("miscellaneoustaxes").value;
+let warranty = document.getElementById("warranty").value;
+let approximatefinalpurchaseprice = document.getElementById("approximatefinalpurchaseprice").value;
+
+let total = currentprice + tax + fees + miscellaneoustaxes + warranty
+document.getElementById("approximatefinalpurchaseprice").innerHTML = currentprice + tax + fees + miscellaneoustaxes + warranty;
+console.log(total);
+
 
