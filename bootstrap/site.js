a59583e118609446fe421edb0601f617564f9fd8
Sean Michaels <michaels.107@osu.edu>
2020-11-14 17:29:15 -0500

    Fixes for html and some CSS stuff

--- bootstrap/site.js
+++ bootstrap/site.js
@@ -1,22 +1,19 @@
-let make = document.getElementById("make").value;
-let model = document.getElementById("model").value;
-let currentprice = document.getElementById("currentprice").value;
+function findFinalPrice() {
+    let make = document.getElementById("make").value;
+    let model = document.getElementById("model").value;
+    let currentprice = document.getElementById("currentprice").value;
 
-let taxpercent = document.getElementById("taxpercent").value;
-let tax = document.getElementById("tax").value;
-let titles = document.getElementById("titles").value;
-let fees = document.getElementById("fees").value;
-let miscellaneoustaxes = document.getElementById("miscellaneoustaxes").value;
-let warranty = document.getElementById("warranty").value;
-let approximatefinalpurchaseprice = document.getElementById("approximatefinalpurchaseprice").value;
-let other = document.getElementById("other").value;
-let year = document.getElementById("year").value;
+    let taxpercent = document.getElementById("taxpercent").value;
+    let tax = document.getElementById("tax").value;
+    let titles = document.getElementById("titles").value;
+    let fees = document.getElementById("fees").value;
+    let miscellaneoustaxes = document.getElementById("miscellaneoustaxes").value;
+    let warranty = document.getElementById("warranty").value;
+    let finalprice = document.getElementById("final price").value;
+    let other = document.getElementById("other").value;
+    let year = document.getElementById("year").value;
 
-totalTax = (taxpercent/100) * parseInt(currentprice);
-
-
-
-function findFinalPrice(){
+    totalTax = (taxpercent / 100) * parseInt(currentprice);
     document.getElementById("tax").innerHTML = totalTax;
     let total = parseFloat(currentprice) + parseFloat(totalTax) + parseFloat(titles) + parseFloat(fees) + parseFloat(miscellaneoustaxes) + parseFloat(warranty) + parseFloat(other);
     console.log("---");
@@ -27,8 +24,6 @@ function findFinalPrice(){
     console.log(miscellaneoustaxes);
     console.log(warranty);
     console.log("---");
-    document.getElementById("approximatefinalpurchaseprice").innerHTML = total;
+    document.getElementById("final price").innerHTML = total;
     console.log(total);
 }
-
-
