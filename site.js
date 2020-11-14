3c82f1fc0232b109867736df5698bf2df0a1bd12
nguyen.1691 <nguyen.1691@osu.edu>
2020-11-14 12:26:51 -0500

    added functionality to the user's input

--- site.js
+++ site.js
@@ -1,16 +1,34 @@
 let make = document.getElementById("make").value;
-console.log(make);
 let model = document.getElementById("model").value;
 let currentprice = document.getElementById("currentprice").value;
+
+let taxpercent = document.getElementById("taxpercent").value;
 let tax = document.getElementById("tax").value;
 let titles = document.getElementById("titles").value;
 let fees = document.getElementById("fees").value;
 let miscellaneoustaxes = document.getElementById("miscellaneoustaxes").value;
 let warranty = document.getElementById("warranty").value;
 let approximatefinalpurchaseprice = document.getElementById("approximatefinalpurchaseprice").value;
+let other = document.getElementById("other").value;
+let year = document.getElementById("year").value;
 
-let total = currentprice + tax + fees + miscellaneoustaxes + warranty
-document.getElementById("approximatefinalpurchaseprice").innerHTML = currentprice + tax + fees + miscellaneoustaxes + warranty;
-console.log(total);
+totalTax = (taxpercent/100) * parseInt(currentprice);
+
+
+
+function findFinalPrice(){
+    document.getElementById("tax").innerHTML = totalTax;
+    let total = parseFloat(currentprice) + parseFloat(totalTax) + parseFloat(titles) + parseFloat(fees) + parseFloat(miscellaneoustaxes) + parseFloat(warranty) + parseFloat(other);
+    console.log("---");
+    console.log(currentprice);
+    console.log(totalTax);
+    console.log(titles);
+    console.log(fees);
+    console.log(miscellaneoustaxes);
+    console.log(warranty);
+    console.log("---");
+    document.getElementById("approximatefinalpurchaseprice").innerHTML = total;
+    console.log(total);
+}
 
 
